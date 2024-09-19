cb99d02862822e581dd3b8e548b0e8d38f1776d5
aibi10 <isingh.abhishek10@gmail.com>
2024-09-19 15:14:35 +0400

    first commit

---
+++ template.py
@@ -0,0 +1,34 @@
+import os
+from pathlib import Path
+
+package_name = "mongodb_connect"
+
+list_of_files = [
+    ".github/workflows/ci.yaml",
+    "src/__init__.py",
+    f"src/{package_name}/__init__.py",
+    f"src/{package_name}/crud.py",
+    "test/unit/__init__.py",
+    "test/unit/unit.py",
+    "test/integration/__init__.py",
+    "test/integration/int.py",
+    "init_setup.sh",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "setup.py",
+    "setup.config",
+    "pyproject.toml",
+    "tox.ini",
+    "experiments/experiments.ipynb"
+]
+
+for file_path in list_of_files:
+        # Convert string to Path object
+        path = Path(file_path)
+        
+        # Create parent directories if they don't exist
+        path.parent.mkdir(parents=True, exist_ok=True)
+        
+        # Create the file
+        path.touch(exist_ok=True)
+        print(f"Created: {path}")
