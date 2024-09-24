c8b02ed10c21eee3d14a551e68c3cfc28c248ac2
aibi10 <isingh.abhishek10@gmail.com>
2024-09-24 10:17:44 +0400

    __init__ file modified again

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
    
    
 
-__version__ = "0.0.4"
+__version__ = "0.0.6"
 REPO_NAME = "monogodbconnector"
 PKG_NAME= "mongodbautomationaibi"
 AUTHOR_USER_NAME = "aibi10"
